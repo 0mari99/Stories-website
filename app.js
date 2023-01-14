419327fb3326b9ab8c0785d72b987263130de75f
0mari99 <abdullah.alomari127@gmail.com>
2023-01-15 00:16:01 +0300

    .env files

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express();
+var dotenv = require('dotenv').config();
+var nano = require('nano')(process.env.baseUrl);
+const baseDB= nano.db.use(process.env.bdName)
 const users = []
-app.listen(3000)
-app.get('/users', (req, res) => {
-    res.json(users)
+app.listen(process.env.port)
+app.post('/users', (req, res) => {
+    var a ={
+        name: "abdullah"
+    }
+    baseDB.insert(a)
+    res.json({})
 })
