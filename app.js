c95ca74feed8b10f42bf2437bfb4b4c962848897
0mari99 <abdullah.alomari127@gmail.com>
2023-01-17 13:25:32 +0300

    Ghassan

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express')
-const app = express();
 var dotenv = require('dotenv').config();
-var nano = require('nano')(process.env.baseUrl);
-const baseDB= nano.db.use(process.env.bdName)
+const app = express();
+// var nano = require('nano')(process.env.baseUrl);
+var nano = require('nano')('http://admin:12345@localhost:5984');
+console.log(nano)
+nano.db.create("tesla");
+var baseDB = nano.db.use("test")
 const users = []
 app.listen(process.env.port)
 app.post('/users', (req, res) => {
     var a ={
         name: "abdullah"
     }
-    baseDB.insert(a)
-    res.json({})
+    baseDB.insert(a, (err, resp)=> {
+        if(err){
+            console.log(err)
+        }
+        res.json(resp)
+    })
+    // res.json({})
 })
